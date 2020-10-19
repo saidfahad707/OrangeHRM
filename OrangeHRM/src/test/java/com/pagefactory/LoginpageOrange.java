17331a1b412f381e8e1ac3bf6c449ef31c077671
fahad <fahad@DESKTOP-2N65I5E.lan>
2020-10-19 19:58:49 -0400

    change orange HRM login feature

--- OrangeHRM/src/test/java/com/pagefactory/LoginpageOrange.java
+++ OrangeHRM/src/test/java/com/pagefactory/LoginpageOrange.java
@@ -15,68 +15,27 @@ public class LoginpageOrange {
 	
 	@FindBy (how= How.XPATH, using = "//*[@id='txtUsername']")
 	private WebElement username;
-	
 
 	@FindBy (how= How.XPATH, using = "//*[@id='txtPassword']")
 	private WebElement password;
-
-	@FindBy (how = How.XPATH, using = "//*[@id=\\'btnLogin\\']")
-	private WebElement singin;
 	
-	@FindBy (how = How.XPATH, using = "//*[@id=\\\"welcome\\\"]")
-	private WebElement logout;
-
-	@FindBy (how = How.XPATH, using = "//*[@id=\\\"welcome-menu\\\"]/ul/li[2]/a")
-	private WebElement click;
-
 	public WebElement getUsername() {
 		return username;
 	}
 
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public void setPassword(WebElement password) {
+		this.password = password;
+	}
 
 	public void setUsername(WebElement username) {
 		this.username = username;
 	}
 
 
-	public WebElement getPassword() {
-		return password;
-	}
-
-
-	public void setPassword(WebElement password) {
-		this.password = password;
-	}
-
-
-	public WebElement getSingin() {
-		return singin;
-	}
-
-
-	public void setSingin(WebElement singin) {
-		this.singin = singin;
-	}
-
-
-	public WebElement getLogout() {
-		return logout;
-	}
-
-
-	public void setLogout(WebElement logout) {
-		this.logout = logout;
-	}
-
-
-	public WebElement getClick() {
-		return click;
-	}
-
-
-	public void setClick(WebElement click) {
-		this.click = click;
-	}
 	
 	
 	
--- OrangeHRM/src/test/java/com/stepDef/LoginSteps.java
+++ OrangeHRM/src/test/java/com/stepDef/LoginSteps.java
@@ -2,10 +2,17 @@ package com.stepDef;
 
 
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import com.pagefactory.LoginpageOrange;
 
@@ -32,8 +39,9 @@ public class LoginSteps {
 
 	@Then("^user Navigate to Page and verify the Page Title is \"([^\"]*)\"$")
 	public void user_Navigate_to_Page_and_verify_the_Page_Title_is(String arg1) throws Throwable {
+//	  driver.getTitle();
 	  String actule = driver.getTitle();
-	  String expected = "Orangehrm";
+	  String expected = "OrangeHRM";
 	  Assert.assertTrue("the titel is Wrong", actule.contains(expected));
 	  
 	  
@@ -44,30 +52,41 @@ public class LoginSteps {
 	@Then("^user enter a valid username and password$")
 	public void user_enter_a_valid_username_and_password() throws Throwable {
 		
-		//		driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys("Admin");
-		//		driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys("admin123");
+//			driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys("Admin");
+//			driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys("admin123");
 		
 		obj.getUsername().sendKeys("Admin");
 		obj.getPassword().sendKeys("admin123");
 		
 	    }
 
+
 	@Then("^user click the Sign in button$")
 	public void user_click_the_Sign_in_button() throws Throwable {
+//		driver.findElement(By.xpath("//*[@id=\'btnLogin\']")).click();	
+//		Assert.assertTrue(2==4);
 		
-//		driver.findElement(By.xpath("//*[@id=\'btnLogin\']")).click();
-	
+		//object.getSignin().click();
+
+		WebElement signin =driver.findElement(By.xpath("//*[@id='btnLogin']"));
+		signin.click();
+		
 	}
 
 	@When("^user click on log out button for Orange Hrm$")
-	public void user_click_on_log_out_button_for_Orange_Hrm() throws Throwable {
-//		WebElement showButton=driver.findElement(By.xpath("//*[@id=\"welcome\"]"));
-//		showButton.click();
+	public void user_click_on_log_out_button_for_Orange_HRM() throws Throwable {
 		
-		Thread.sleep(4000);
+		WebElement welcome =driver.findElement(By.xpath("//*[contains(text(),'Welcome')]"));
+		welcome.click();
+		WebElement logout =driver.findElement(By.xpath("//*[contains(text(),'Logout')]"));
 		
-		//driver.findElement(By.xpath("//*[@id=\"welcome-menu\"]/ul/li[2]/a")).click();
-	    
+//		ExplicitWait
+		
+
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+				  .withTimeout(30, TimeUnit.SECONDS)
+				  .pollingEvery(1, TimeUnit.SECONDS);
+		wait.until(ExpectedConditions.visibilityOf(logout)).click();
 		
 	}
 
